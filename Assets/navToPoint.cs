b3fe31e715f2f261653e2996339130c7b2027b5f
Ryan Dougherty <ryand626@gmail.com>
2014-05-17 17:16:51 -0400

    First Commit
    Point and click raycast navigation

---
+++ Assets/navToPoint.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class navToPoint : MonoBehaviour {
+
+	public Transform target;
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = gameObject.GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target) {
+			if(Input.GetMouseButtonDown(0)){
+				agent.SetDestination(target.position);
+			}
+		}
+	}
+}
---
+++ Assets/point.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class point : MonoBehaviour {
+
+	private RaycastHit hit;
+	private Ray mouseRay;
+	
+	// Update is called once per frame
+	void Update () {
+		mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast (mouseRay, out hit,100)) {
+			if(hit.collider.name != "raypoint"){
+				print ("Hit " + hit.collider.name);
+				transform.position = (Vector3)(hit.point);
+				transform.rotation = Quaternion.LookRotation(hit.normal);
+			}
+		}else{
+			transform.position = new Vector3(200f,200f,200f);
+		}
+	}
+}
