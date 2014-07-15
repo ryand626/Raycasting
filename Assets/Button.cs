d908226e26565aea43c3ab338347ebb1da8e26bf
Ryan Dougherty <Ryan@GLaDOS.local>
2014-07-14 23:55:39 -0400

    button functionality
    started messing around with point and click button interfaces combined
    with keypresses

---
+++ Assets/Button.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+	public bool on;
+	public bool textureDebounce;
+
+	// Use this for initialization
+	void Start () {
+		textureDebounce = false;
+		on = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (on) {
+			if(textureDebounce != on){
+				renderer.material = Resources.Load<Material> ("select");
+				textureDebounce = on;
+			}
+		} else {
+			if(textureDebounce != on){
+				renderer.material = Resources.Load<Material>("deselect");
+				textureDebounce = on;
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider collisionInfo){
+		on = true;
+	}
+
+	void OnTriggerExit(Collider collisionInfo){
+		on = false;
+	}
+}
---
+++ Assets/makeCube.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class makeCube : MonoBehaviour {
+
+	private Button myButton;
+	private bool create;
+
+	// Use this for initialization
+	void Start () {
+		create = false;
+		myButton = gameObject.GetComponent<Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space) && myButton.on == true) {
+			create = true;
+		}
+
+		if (create == true && myButton.on == true) {
+			print ("MAKIN A THING");
+			GameObject newCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			newCube.transform.position = transform.position + new Vector3(0,1.2f,0);
+			create = false;
+		}
+	}
+}
